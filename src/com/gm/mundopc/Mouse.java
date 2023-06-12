f30952fbc07eb55036cd3e18a450eeaf03e6b181
LautaroNahuelNunez <lautaronunez@live.com>
2023-06-12 20:25:37 -0300

    Creando clase Mouse

---
+++ src/com/gm/mundopc/Mouse.java
@@ -0,0 +1,25 @@
+package com.gm.mundopc;
+
+public class Mouse extends DispositivoEntrada{
+    private int idMouse;
+    private static int contadorMouse;
+
+    public Mouse(String tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        this.idMouse = ++contadorMouse;
+    }
+
+    public int getIdMouse() {
+        return idMouse;
+    }
+
+    @Override
+    public String toString() {
+        return "Mouse: {idMouse : " + idMouse + ", " + super.toString() + "}"; 
+    }
+
+
+
+
+
+}
