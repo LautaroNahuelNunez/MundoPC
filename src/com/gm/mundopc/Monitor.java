865491947d10fd7c45a27292a77d679474f2d50f
LautaroNahuelNunez <lautaronunez@live.com>
2023-06-12 23:33:40 -0300

    Creando clase Monitor

---
+++ src/com/gm/mundopc/Monitor.java
@@ -0,0 +1,39 @@
+package com.gm.mundopc;
+
+public class Monitor {
+    private int idMonitor;
+    private String marca;
+    private double tamanio;
+    private static int contadorMonitores;
+
+    private Monitor(){
+        this.idMonitor = ++Monitor.contadorMonitores;
+    }
+
+    public Monitor(String marca, double tamanio ){
+        this(); //llamada al constructor vacío para llevar la cuenta de monitores
+        this.marca = marca;
+        this.tamanio = tamanio;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public double getTamanio() {
+        return this.tamanio;
+    }
+
+    @Override
+    public String toString() {
+        return "Monitor {marca: " + marca + ", tamanio: " + tamanio + "}";
+    }
+}
