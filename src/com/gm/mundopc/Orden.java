89607974fd906c7322ff06df05e853beff2fa99b
LautaroNahuelNunez <lautaronunez@live.com>
2023-06-13 22:43:28 -0300

    Creando clase Orden

---
+++ src/com/gm/mundopc/Orden.java
@@ -0,0 +1,31 @@
+package com.gm.mundopc;
+
+public class Orden {
+    private final int idOrden;
+    private Computadora computadoras[];
+    private static int contadorOrdenes;
+    private int contadorComputadoras;
+    private static final int MAX_COMPUTADORAS = 5;
+
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.computadoras = new Computadora[Orden.MAX_COMPUTADORAS];
+    }
+
+    public void agregarComputadora(Computadora computadora){
+        if(this.contadorComputadoras > Orden.MAX_COMPUTADORAS){
+            this.computadoras[this.contadorComputadoras++] = computadora;
+        }else{
+            System.out.println("SE HA ALCANZADO EL MÁXIMO DE COMPUTADORAS: " + Orden.MAX_COMPUTADORAS);
+        }
+    }
+
+    public void mostrarOrden(){
+        System.out.println("Orden #" + this.idOrden);
+        System.out.println("Computadoras de la orden #" + this.idOrden);
+        for(int i=0; i<this.contadorComputadoras; i++){
+            System.out.println(this.computadoras[i]);
+        }
+    }
+
+}
